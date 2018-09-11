bef548a5aa92f6f21c781a9b0ee68e6aae02a7cd
Maher Rahman <MaherRahman1@gmail.com>
2018-09-11 18:34:13 -0500

    MainActivity java file

---
+++ MainActivity.java
@@ -0,0 +1,58 @@
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TicTacToe game;
+    private Button[] buttons;
+    
+    private TextView statusTextView;
+    private Button resetButton;
+    
+    private boolean isHumanTurn;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        statusTextView = findViewById(R.id.textView);
+        resetButton = findViewById(R.id.resetButton);
+    
+    }
+    
+    public void onClickButton0 (View view) {
+    
+    }
+    
+    public void onClickButton1 (View view) {
+    
+    }
+    
+    public void onClickButton2 (View view) {
+    
+    }
+    
+    public void onClickButton3 (View view) {
+    
+    }
+    
+    public void onClickButton4 (View view) {
+    
+    }
+    
+    public void onClickButton5 (View view) {
+    
+    }
+    
+    public void onClickButton6 (View view) {
+    
+    }
+    
+    public void onClickButton7 (View view) {
+    
+    }
+    
+    public void onClickButton8 (View view) {
+    
+    }
